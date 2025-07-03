d8d991eef507898204fa577da669a323b51a11d9
NeilRow56 <neilrowland56@gmail.com>
2025-07-03 10:19:46 +0100

    Tweaks to Better-Auth setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,7 @@
-import { Logout } from '@/components/logout'
-
 export default function Dashboard() {
   return (
     <div className='flex h-screen flex-col items-center justify-center'>
-      <Logout />
+      Dasboard page
     </div>
   )
 }
